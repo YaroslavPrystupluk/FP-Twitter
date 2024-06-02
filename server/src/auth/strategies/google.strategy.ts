dbbcb42a6c322bd959b32a455035f87f3b8721aa
Iaroslav05 <tyucha_05@ukr.net>
2024-06-03 00:16:53 +0300

    dd

--- server/src/auth/strategies/google.strategy.ts
+++ server/src/auth/strategies/google.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-oauth20';
+import { Profile, Strategy } from 'passport-google-oauth20';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
@@ -17,7 +17,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   async validate(
     accessToken: string,
     refreshToken: string,
-    profile,
+    profile: Profile,
     done: (err: any, user: any, info?: any) => void,
   ) {
     const { name, emails, photos } = profile;
